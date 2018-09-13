628f5492a24b9adcfb59dcc0700589e155bed0d4
zhaojintian <729234283@qq.com>
2018-09-13 09:39:15 +0800

    update

---
+++ src/components/swiper/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="c-swiper">
+    <swiper
+      :circular="true"
+      :autoplay="true"
+      :interval="2500"
+      @change="handleChange">
+      <swiper-item
+        v-for="(item, index) in items"
+        :key="index">
+        <image
+          mode="aspectFill"
+          :src="item.image"
+          @click="handleClick(item)" />
+      </swiper-item>
+    </swiper>
+    <div class="cc-indicator">
+      <span
+        v-for="(item, index) in indicatorItems"
+        :key="index"
+        :class="[ 'cc-indicator__item', { 'is-active': item === current } ]"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      items: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    },
+    data () {
+      return {
+        current: 0
+      }
+    },
+    computed: {
+      indicatorItems () {
+        return Array.from({ length: this.items.length }, (v, k) => k)
+      }
+    },
+    methods: {
+      handleChange (e) {
+        this.current = e.mp.detail.current
+      },
+      handleClick () {}
+    }
+  }
+</script>
+
+<style lang="scss" src="./styles.scss"></style>
