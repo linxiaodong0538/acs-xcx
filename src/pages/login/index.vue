659d69148c3dd9fece2dc96a65a2ee2c61c2c4ad
lx160538 <734127061@qq.com>
2018-09-14 17:45:51 +0800

    a

---
+++ src/pages/login/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="p-tab-bar-my">
+    <button open-type="getUserInfo" @bindgetuserinfo="getUserInfo" style="margin-top:300rpx">登陆 </button>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    // this.getUserInfo()
+  },
+  data () {
+    return {}
+  },
+  methods: {
+    getUserInfo (e) {
+      console.log(e)
+      wx.login({
+        success: res => {
+          console.log(1)
+          wx.getUserInfo({
+            success: res => {
+              console.log(2)
+            }
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./styles.scss"></style>
