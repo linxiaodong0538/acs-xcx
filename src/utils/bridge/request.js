379aa087ed14f395e83b185a980193857cd9fd98
zhaojintian <729234283@qq.com>
2018-09-14 13:42:40 +0800

    update

--- src/utils/bridge/request.js
+++ src/utils/bridge/request.js
@@ -1,4 +1,8 @@
+import consts from '../consts/index'
+
 export default options => {
+  options.url = `${consts.BASE_URL}/${options.url}`
+
   return new Promise((resolve, reject) => {
     /* global wx */
     wx.request({ ...options, success: resolve, fail: reject })
