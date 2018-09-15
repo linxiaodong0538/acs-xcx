7e21a2a8bc68271b71cee00455288b302e7a8ab5
linxin <734127061@qq.com>
2018-09-15 13:22:57 +0800

    更新

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="p-tab-bar-my">
     <button open-type="getUserInfo" @bindgetuserinfo="getUserInfo" style="margin-top:300rpx">登陆 </button>
+    <button @click="aa">1</button>
   </div>
 </template>
 
 <script>
 export default {
-  created () {
-    this.getUserInfo()
+  async created () {
+    await this.getUserInfo()
   },
   data () {
     return {}
@@ -17,10 +18,12 @@ export default {
       wx.login({
         success: res => {
           const code = res.code
+          // 查看是否授权
           wx.getSetting({
             success: res => {
               console.log(res)
               if (res.authSetting['scope.userInfo']) {
+                // 已经授权，可以直接调用 getUserInfo 获取头像昵称
                 wx.getUserInfo({
                   success: res => {
                     let param = {
@@ -33,25 +36,47 @@ export default {
                       data: param,
                       header: {
                         'Content-Type': 'application/json'
-                      },
-                      success: res => {
-                        console.log(res)
                       }
+                    }).then(v => {
+                      console.log(1, v)
                     })
                     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
                     // 所以此处加入 callback 以防止这种情况
                     if (this.userInfoReadyCallback) {
                       this.userInfoReadyCallback(res)
                     }
+                    wx.navigateTo({
+                      url: '../tab-bar/categories/main.js'
+                    })
                   }
                 })
               } else {
                 console.log(2)
               }
+            },
+            fail: res => {
+              console.log('err')
+              wx.showToast({
+                title: '您需要授权才能进行下一步的操作,请点击登陆授权',
+                icon: 'none',
+                duration: 2000
+              })
             }
           })
         }
       })
+    },
+    aa: res => {
+      wx.switchTab({
+        url: '../../pages/tab-bar/index/main.js',
+        success: e => {
+          console.log(e)
+        },
+        fail: err => {
+          console.log(err)
+        }
+      }
+      )
     }
   }
 }
--- src/pages/tab-bar/index/index.vue
+++ src/pages/tab-bar/index/index.vue
@@ -6,22 +6,40 @@
     ]" />
     <div class="c-panel">
       <div class="c-panel__head">
-        <div class="c-panel__title u-c3 u-fs36 u-fwb">最热测试</div>
+        <div class="c-panel__title c3 fs36 u-fwb">最热测试</div>
       </div>
       <div class="c-panel__body">
         <scroll-view
           class="c-list1-scroll"
           scroll-x>
           <div class="c-list1" :style="{ width: (225 + 10) * 4 + 20 + 'rpx' }">
-            <div
-              v-for="(item, index) in [0, 1, 2, 3]"
-              :key="index"
-              class="c-list1__item">
+            <div class="c-list1__item">
               <image
                 class="c-list1__image"
                 src="http://localhost:88/demos/pages/tab-bar/index/list1/1.jpg" />
-              <div class="c-list1__title u-c3 u-fs28 u-lh-fs34 u-fwb">{{ item }} - 这里是标题</div>
-              <div class="c-list1__price u-c2 u-fs26 u-lh-1 u-fwb">¥11</div>
+              <div class="c-list1__title c3 fs28 lh-fs34">这里是标题</div>
+              <div class="c-list1__price c2 fs26">¥11</div>
+            </div>
+            <div class="c-list1__item">
+              <image
+                class="c-list1__image"
+                src="http://localhost:88/demos/pages/tab-bar/index/list1/1.jpg" />
+              <div class="c-list1__title c3 fs28 lh-fs34">这里是标题</div>
+              <div class="c-list1__price c2 fs26">¥11</div>
+            </div>
+            <div class="c-list1__item">
+              <image
+                class="c-list1__image"
+                src="http://localhost:88/demos/pages/tab-bar/index/list1/1.jpg" />
+              <div class="c-list1__title c3 fs28 lh-fs34">这里是标题</div>
+              <div class="c-list1__price c2 fs26">¥11</div>
+            </div>
+            <div class="c-list1__item">
+              <image
+                class="c-list1__image"
+                src="http://localhost:88/demos/pages/tab-bar/index/list1/1.jpg" />
+              <div class="c-list1__title c3 fs28 lh-fs34">这里是标题</div>
+              <div class="c-list1__price c2 fs26">¥11</div>
             </div>
           </div>
         </scroll-view>
@@ -29,18 +47,33 @@
     </div>
     <div class="c-panel">
       <div class="c-panel__head m-underline">
-        <div class="c-panel__title u-c3 u-fs36 u-fwb">朋友都在测</div>
-        <div class="c-panel__more u-c5 u-fs26 u-vc">更多</div>
+        <div class="c-panel__title c3 fs36 u-fwb">朋友都在测</div>
+        <div class="c-panel__more c5 fs26 u-vc">更多</div>
       </div>
       <div class="c-panel__body c-list">
-        <div
-          v-for="(item, index) in [0, 1, 2, 3]"
-          :key="index"
-          class="c-list__item">
-          <div class="c-list__title u-c3 u-fs30 u-lh-1 u-fwb u-to">{{ item }} - 是啊啊电风扇发是啊啊电风扇发是啊啊电风扇发是啊啊电风扇发</div>
-          <div class="c-list__desc u-c5 u-fs22 u-lh-1 u-to">胜多负少的</div>
-          <div class="c-list__price u-c2 u-fs26 u-lh-1 u-fwb">¥11</div>
-          <div class="c-list__addon u-c5 u-fs22 u-lh-1">11人已测</div>
+        <div class="c-list__item">
+          <div class="c-list__title c3 fs30 u-lh-1 u-to">是啊啊电风扇发是啊啊电风扇发是啊啊电风扇发是啊啊电风扇发</div>
+          <div class="c-list__desc c5 fs22 u-lh-1 u-to">胜多负少的</div>
+          <div class="c-list__price u-lh-1 c2 fs26">¥11</div>
+          <div class="c-list__addon u-lh-1 c5 fs22">11人已测</div>
+          <image
+            class="c-list__image u-vc"
+            src="http://localhost:88/demos/pages/tab-bar/index/list/1.jpg" />
+        </div>
+        <div class="c-list__item">
+          <div class="c-list__title c3 fs30 u-lh-1 u-to">是啊啊电风扇发是啊啊电风扇发是啊啊电风扇发是啊啊电风扇发</div>
+          <div class="c-list__desc c5 fs22 u-lh-1 u-to">胜多负少的</div>
+          <div class="c-list__price u-lh-1 c2 fs26">¥11</div>
+          <div class="c-list__addon u-lh-1 c5 fs22">11人已测</div>
+          <image
+            class="c-list__image u-vc"
+            src="http://localhost:88/demos/pages/tab-bar/index/list/1.jpg" />
+        </div>
+        <div class="c-list__item">
+          <div class="c-list__title c3 fs30 u-lh-1 u-to">是啊啊电风扇发是啊啊电风扇发是啊啊电风扇发是啊啊电风扇发</div>
+          <div class="c-list__desc c5 fs22 u-lh-1 u-to">胜多负少的</div>
+          <div class="c-list__price u-lh-1 c2 fs26">¥11</div>
+          <div class="c-list__addon u-lh-1 c5 fs22">11人已测</div>
           <image
             class="c-list__image u-vc"
             src="http://localhost:88/demos/pages/tab-bar/index/list/1.jpg" />
@@ -64,7 +97,7 @@
     },
     async created () {
       this.items = await this.$bridge.request({
-        url: ''
+        url: 'z'
       })
 
       // console.log(this.items)
