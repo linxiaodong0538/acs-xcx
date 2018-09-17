169ce044a18766cd134cf082322df56c7e7684d3
zhaojintian <729234283@qq.com>
2018-09-18 01:12:46 +0800

    add plugin

--- src/utils/plugins/global.js
+++ src/utils/plugins/global.js
@@ -1,14 +1,18 @@
 import time from 'jt-time'
 import consts from '../consts/index'
 import helpers from '../helpers/base'
-import bridge from '../bridge/index'
+import bridge from '../bridge'
+import auth from '../auth'
+import request from '../request'
 
 export default {
   install (Vue) {
+    Vue.prototype.$time = time
     Vue.prototype.$consts = consts
     Vue.prototype.$helpers = helpers
     Vue.prototype.$bridge = bridge
-    Vue.prototype.$time = time
+    Vue.prototype.$auth = auth
+    Vue.prototype.$request = request
 
     Vue.filter('time', val => time.getTime(val))
     Vue.filter('date', val => time.getDate(val))
