00f7cb685e8f5bbfcac4e1217dc50b5ccea514b1
zhaojintian <729234283@qq.com>
2018-09-20 14:30:43 +0800

    详情页

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="p-detail">
+    <image
+      class="pb-banner"
+      src="http://localhost:88/demos/pages/detail/banner.png" />
+    <div class="o-box u-mb-14">
+      <div class="pb-title u-c3 u-fs34 u-lh-1 u-tac u-fwb">爱情风格评估</div>
+      <div class="pb-subtitle u-c5 u-fs24 u-lh-1 u-tac">测测你向往的爱情是什么样的？</div>
+      <div class="pb-body u-c3 u-fs28">夏天的脚步慢慢逼近，你准备好了吗？说到夏天，那当然是阳光海滩以及爱情了。这个夏天，你的爱情会有遗憾吗？测试小屋为你揭秘。</div>
+      <div class="pb-price u-c2 u-fs30 u-lh-1 u-tac u-fwb">¥9.9</div>
+      <div class="pb-buttons o-button1-group o-grid">
+        <div class="o-grid__cell">
+          <div class="c-button1 is-left">发士大夫地</div>
+        </div>
+        <div class="o-grid__cell">
+          <div class="c-button1 is-right">发士大夫地</div>
+        </div>
+      </div>
+    </div>
+    <div class="pb-notice o-box">
+      <div class="pb-notice__title u-c u-fs30 u-lh-1 u-fwb">测试须知</div>
+      <text class="pb-notice__body u-c3 u-fs28">
+        1.点击右上角的分享，分享给好友，获得2位以上好友的砍价成功即可免费测一次；\n
+        2.本测试需要花费5分钟左右就可以出结果；\n
+        3.付费成功后自动给跳转到答题页，注意保持网络畅通；\n
+        4.为保证结果的准确性，测试只能做一次；\n
+        5.为保证结果的准确性，测试只能做一次；
+      </text>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" src="./styles.scss"></style>
