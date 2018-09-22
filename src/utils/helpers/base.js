eab33d8d081c32c4ee0dec2c4d4efa7d5a19cace
zhaojintian <729234283@qq.com>
2018-09-22 17:59:26 +0800

    update

--- src/utils/helpers/base.js
+++ src/utils/helpers/base.js
@@ -1,14 +1,3 @@
-import consts from '@/utils/consts'
 import helpers from 'jt-helpers'
 
-export default {
-  ...helpers,
-  getFileURLById (id) {
-    return `${consts.BASE_URL}/apis/v1/files/${id}`
-  },
-  getItemById (items, id) {
-    return items && items.length
-      ? (items.find(item => +item.id === +id) || {})
-      : {}
-  }
-}
+export default helpers
