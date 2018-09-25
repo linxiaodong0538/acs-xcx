5aacbb43a61899bc92051943e8b116af8f049164
lx160538 <734127061@qq.com>
2018-09-25 18:23:36 +0800

    更新

--- src/pages/topic/index.vue
+++ src/pages/topic/index.vue
@@ -6,10 +6,16 @@
       :interval="2500"
       @change="handleChange" class="pb-siwper-wrapper u-bg-c1">
       <swiper-item
-        v-for="(item, index) in test"
+        v-for="(item, index) in topicData.questions"
         :key="index">
           <div class="pb-siwper-wrapper__content">
-            <p class="u-tac">{{item.title}}</p>
+            <p class="u-tac pb-siwper-wrapper__title fs36">{{item.subject}}</p>
+            <radio-group class="pb-siwper-wrapper__radio-group" @change="radioChange">
+              <label class="pb-siwper-wrapper__radio" v-for="(options, i) in item.options" :key="i">
+                <span class="fs32">{{options.option}}</span>
+                <radio :value="options.option" :checked="true" color="#FED02F" :data-id="options.id"/> 
+              </label>
+            </radio-group>
           </div>
       </swiper-item>
     </swiper>
@@ -24,19 +30,33 @@
 export default {
   data () {
     return {
-      test: [{title: '测试1测试1'}, {title: '测试2'}, {title: '测试3'}, {title: '测试4'}],
+      topicData: {},
       current: 0
     }
   },
   computed: {
     pageNum () {
-      return this.test.length
+      return this.topicData.questions.length
     }
   },
   methods: {
     handleChange (e) {
       this.current = e.mp.detail.current
+    },
+    async getAnswer () {
+      let res = await this.$bridge.request({
+        url: 'project/gotest/1'
+      })
+      if (res.data.code === 0) {
+        this.topicData = res.data.data[0]
+      }
+    },
+    radioChange (e) {
+      console.log(e.mp)
     }
+  },
+  async created () {
+    await this.getAnswer()
   }
 }
 </script>
